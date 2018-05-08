9aab73c133952f12bc14b3687dacde508c3e20ae
Travis Wright <twright@microsoft.com>
2018-05-07 17:32:58 -0700

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM microsoft/dotnet:2.0-sdk AS build
+WORKDIR /app
+
+# copy csproj and restore as distinct layers
+COPY *.csproj .
+RUN dotnet restore
+
+# copy and build everything else
+
+COPY . .
+RUN dotnet publish -c Release -o out
+
+
+FROM microsoft/dotnet:2.0-runtime AS runtime
+WORKDIR /app
+COPY --from=build /app/out .
+ENTRYPOINT ["dotnet", "./netcore-mssql-connect.dll"]
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text;
+using System.Data.SqlClient;
+using System.Threading;
+
+namespace SqlServerSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Build connection string
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.DataSource = "172.30.96.55";    // update me
+                builder.UserID = "sa";                  // update me
+                builder.Password = "Yukon900";          // update me
+                builder.InitialCatalog = "master";
+
+                while(true)
+                {
+                    try{
+                        // Connect to SQL
+                        Thread.Sleep(1000);
+                        Console.Write("Connecting to SQL Server ... ");
+                        using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                        {
+                            connection.Open();
+                            String sql = "SELECT @@servername";
+                            using (SqlCommand command = new SqlCommand(sql, connection))
+                            {
+                                using (SqlDataReader reader = command.ExecuteReader())
+                                {
+                                    while (reader.Read())
+                                    {
+                                        Console.WriteLine("Currently connected server name is: {0}", reader.GetString(0));
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Lost connection!!  Retrying to connect...");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }            
+        }
+    }
+}
